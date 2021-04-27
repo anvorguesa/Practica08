e00a1d3d51b23ac7e416d01bf44d6a8c21b1849b
anvorguesa <srliposa@gmail.com>
2021-04-27 18:40:35 -0500

    create index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Practica08.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult ProductosServicios()
         {
             return View();
         }
