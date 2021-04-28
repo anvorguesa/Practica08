92d31c4f80d61bc425e4c1a10401d0a7495f7dbf
anvorguesa <srliposa@gmail.com>
2021-04-27 19:34:22 -0500

    solucionando errores pequeños

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -1,10 +1,10 @@
-namespace Practica08.Controllers
+namespace Practica08.Models
 {
     public class Contacto
     {
-        public string FirstName{get;set;}
-        public string LastName{get;set;}
-        public string Email{get;set;}
-        public string Phone{get;set;}  
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
